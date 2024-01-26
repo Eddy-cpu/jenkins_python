07ca5a0429d95345a4fa51c1c79c731db36f34f9
Eddy-cpu <67406533+Eddy-cpu@users.noreply.github.com>
2024-01-26 10:43:37 +0800

    Update test_menu_selection.py

--- test_menu_selection.py
+++ test_menu_selection.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 from menu_selection import SystemMonitor
 
 class TestSystemMonitor(unittest.TestCase):
-   @patch('psutil.disk_usage')
-   def test_check_disk_usage(self, mock_disk_usage):
+    @patch('psutil.disk_usage')
+    def test_check_disk_usage(self, mock_disk_usage):
         mock_disk_usage.return_value.percent = 25
         monitor = SystemMonitor()
         self.assertTrue(monitor.check_disk_usage())
